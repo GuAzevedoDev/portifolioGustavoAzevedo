37541646645c8061e00632239af126b1a4c2af46
Gustavo Azevedo <gustavoazevedo.code@gmail.com>
2025-09-16 23:13:09 -0300

    finalizando responsivo

--- Javascript/script.js
+++ Javascript/script.js
@@ -6,3 +6,11 @@ let body = document.body;
 function alteraTema() {
   body.classList.toggle("ligth");
 }
+
+let temaMob = document
+  .getElementById("temaMobile")
+  .addEventListener("click", alteraTema);
+
+function alteraTema() {
+  body.classList.toggle("ligth");
+}
