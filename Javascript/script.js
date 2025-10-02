df67a13ef8ab6d639516717313a15c5404dc3fac
GUSTAVO_AZEVEDO_OLIVEIRA <22403140@ALFAF19.FLORESTA>
2025-10-02 08:57:40 -0300

    projetos

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,4 @@
-let tema = document
-  .getElementById("tema")
-  .addEventListener("click", alteraTema);
+let tema = document.getElementById("tema").addEventListener("click", alteraTema);
 let body = document.body;
 
 function alteraTema() {
@@ -14,6 +12,7 @@ let temaMob = document
 function alteraTema() {
   body.classList.toggle("ligth");
 }
+
 const check = document.getElementById("check-mobile");
 const btn = document.querySelector(".btn-baixo");
 
@@ -21,15 +20,15 @@ check.addEventListener("change", () => {
   if (check.checked) {
     btn.style.zIndex = "-1";
   } else {
-    btn.style.zIndex = "2"
+    btn.style.zIndex = "2";
   }
 });
 const header = document.querySelector("#header");
 const novaLargura = window.innerWidth;
-window.addEventListener('wheel', function(event) {
-  if (event.deltaY < 0 && novaLargura < 768) {
-    header.classList.remove("desaparece-header")
-  } else if (event.deltaY > 0 && novaLargura < 768) {
-    header.classList.add("desaparece-header")
+window.addEventListener("wheel", function (event) {
+  if (event.deltaY < 0 && novaLargura > 768) {
+    header.classList.remove("desaparece-header");
+  } else if (event.deltaY > 0 && novaLargura > 768) {
+    header.classList.add("desaparece-header");
   }
 });
