cc3d461526ac035b57b7b68f9c471128cc27c6c5
Gustavo Azevedo <gustavoazevedo.code@gmail.com>
2025-11-05 23:52:37 -0300

    cursos

--- Javascript/script.js
+++ Javascript/script.js
@@ -105,3 +105,18 @@ function AbrirmodalProjetos() {
 }
 
 AbrirmodalProjetos();
+
+document.querySelectorAll(".tag-html").forEach((tag) => {
+  tag.addEventListener("mousemove", (e) => {
+    const rect = tag.getBoundingClientRect();
+    const x = 100 - ((e.clientX - rect.left) / rect.width) * 100; 
+    const y = ((e.clientY - rect.top) / rect.height) * 100;
+    tag.style.setProperty("--x", `${x}%`);
+    tag.style.setProperty("--y", `${y}%`);
+    tag.style.opacity = 0.3;
+  });
+
+  tag.addEventListener("mouseleave", () => {
+    tag.style.opacity = 0.08;
+  });
+});
